3944751c8abd68d4c024d6554ff302c07c99d772
lynix757 <pornsak.backup@gmail.com>
2021-04-07 18:22:38 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,26 +2,39 @@
 
 RETENTION=7
 CDATE=$(date +"%Y%m%d-%H%M") # Current date
+
 SPATH="./source"
 DPATH="./destination"
+LOGPREFIX="log1"
+
 FSRC="$SPATH/file1.txt" # source 
-FDST="$DPATH/archive_log-$CDATE.tar.gz" # destination
+FDST="$DPATH/$LOGPREFIX-$CDATE.tar.gz" # destination
 
 # Delete file older than xx Day
-#find $DPATH/*.tar.gz -mtime +1 -delete
-if [ $1 == "DELETEALLARCHIVE" ] 
-then
-    #rm -f "$DPATH/*.tar.gz"
-    find $DPATH/*.tar.gz -delete
-else
+case $1 in
+
+  ON)
+    echo "Normal"
     echo "$FDST"
     tar -zcvf $FDST $FSRC
+
     # Check
-    echo ""
-    echo "# list file #"
+    #echo ""
+    #echo "# list file #"
     tar -tf $FDST
-    ls -lht $DPATH/*.tar.gz
-    find $DPATH/*.tar.gz -mtime +$RETENTION -delete
-fi
 
+    #ls -lht $DPATH/*.tar.gz
+    find $DPATH/$LOGPREFIX*.tar.gz -mtime +$RETENTION -delete
+    ;;
 
+  DELETEALLARCHIVE)
+    echo "Delete All"
+    find $DPATH/$LOGPREFIX*.tar.gz -delete
+    ;;
+
+  *)
+    echo ""
+    echo "ON - Normal"
+    echo "DELETEALLARCHIVE - Delete all archive log"
+    ;;
+esac
